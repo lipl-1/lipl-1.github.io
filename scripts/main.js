651169fd68e0d395fb1f10dec4a4f13aecac72c3
lipl-3 <73384224+lipl-1@users.noreply.github.com>
2020-10-24 21:18:05 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
 document.querySelector('html').onclick = function() {
-    alert('别戳我，我怕疼。');
+    alert('别戳我，我怕疼！小气鬼');
 }
